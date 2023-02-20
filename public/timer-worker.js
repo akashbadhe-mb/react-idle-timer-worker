d814a71aec36e3925799b78c3f699effe7198498
akashbadhe-mb <akash.badhe@mindbodyonline.com>
2023-02-20 10:03:57 +0530

    Updated timer logic to use 2 timers

--- public/timer-worker.js
+++ public/timer-worker.js
@@ -5,38 +5,46 @@ let countDownTimer;
 let lastInteractionTime;
 
 const startTimer = () => {
-  countDownOutStarted = false;
-  countTimer = countDownOutInterval;
   if (timer) {
     clearInterval(timer);
     clearInterval(countDownTimer);
   }
   timer = setInterval(() => {
     postMessage(`updateLastInteractionTime`);
-    const diff = new Date().valueOf() - (+lastInteractionTime);
-    if (diff < timeOutInterval) {
-      postMessage("onActive");
-    } else if(!countDownOutStarted) {
-      countDownOutStarted = true;
+    const currentTime = new Date().valueOf();
+    const diff = currentTime - (+lastInteractionTime);
+    if (diff > timeOutInterval) {
       postMessage("onIdle");
-    } else {
-      minutes = parseInt(countTimer / 60, 10);
-      seconds = parseInt(countTimer % 60, 10);
-
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      postMessage(`countDown:${minutes + ":" + seconds}`);
-      if (--countTimer < 0) {
-          clearInterval(timer);
-          postMessage('countDownCompleted');
-      }
+      createCountDown();
     }
-  }, 1000);
+  }, 60000);
 };
 
+const createCountDown = () => {
+  if (countDownTimer) {
+    clearInterval(countDownTimer);
+  }
+  if (timer) {
+    clearInterval(timer);
+  }
+  let countTimer = countDownOutInterval;
+  countDownTimer = setInterval(() => {
+    let minutes = parseInt(countTimer / 60, 10);
+    let seconds = parseInt(countTimer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    postMessage(`countDown:${minutes + ":" + seconds}`);
+    if (--countTimer < 0) {
+      clearInterval(countDownTimer);
+      postMessage('countDownCompleted');
+    }
+  }, 1000);
+}
+
 onmessage = (e) => {
-  if(e.data.key === 'startTimer') {
+  if (e.data.key === 'startTimer') {
     postMessage("onActive");
     lastInteractionTime = e.data.value || new Date().valueOf();
     startTimer();
